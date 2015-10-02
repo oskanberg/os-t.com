d37ac0e8e3602b4da33bcd9d1b37fc694cad9510
Oliver Skanberg-Tippen <oliverskanbergtippen@gmail.com>
2015-10-02 17:31:09 +0100

    Add feature to git pull on interval (to update articles easier)

--- serve.go
+++ serve.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"time"
 )
+
+func gitPull() {
+	log.Println("Git Pull")
+
+	cmd := "git"
+	args := []string{"pull"}
+
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		// os.Exit(1)
+	}
+
+}
 
 func main() {
 	port := flag.String("p", "8080", "The port on which to serve requests.")
+	gitPullInterval := flag.Int("interval", 0, "Number of minutes between Git Pulls (<1 for off)")
 	flag.Parse()
+
+	if *gitPullInterval > 0 {
+
+		gitPull()
+
+		ticker := time.NewTicker(time.Duration(*gitPullInterval) * time.Minute)
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					gitPull()
+				}
+			}
+		}()
+	}
 
 	log.Printf("Serving on port %s\n", *port)
 	panic(http.ListenAndServe(":"+*port, http.FileServer(http.Dir("."))))
